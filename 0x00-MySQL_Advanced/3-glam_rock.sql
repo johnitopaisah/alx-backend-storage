f35a6fe5908738d7b10c377044a55a462722b045
isahjohna <isahjohna@gmail.com>
2024-10-17 14:57:53 +0200

    2-fans.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Lists all bands with Blam rock as their main style, ranked by their longevity
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
 -- Column names must be: band_name and lifespan (in years)
--- Use attributes formed and split for computing the lifespam
+-- use attributes formed and split for computing the lifespan
 
 SELECT band_name, (IFNULL(split, 2022) - formed)
-AS lifespam FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
